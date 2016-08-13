64bc31d67c4b7273c09a9685bee386ee5a2bb498
Michael Q <MichaelQuiban@outlook.com>
2016-08-13 09:41:20 -0700

    Added JSON, CSS, HTML.
    Users are now able to search for a movie title and receive the titles
    under the submit button.
    
    Styling leaves much too be desired.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-console.log("hello world!");
+// This function directly below us uses jQuery instead of JSON to get data.
+
+//	$.get('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+//   console.log(data);
+//	  }, 'json')
+
+$(function(){
+  $('#search-term').submit(function(event){
+    event.preventDefault();
+    var searchTerm = $('#query').val();
+    getRequest(searchTerm);
+  });
+});
+
+function getRequest(searchTerm){
+  var params = {
+    s: searchTerm,
+    r: 'json'
+  };
+  url = 'http://www.omdbapi.com';
+
+  $.getJSON(url, params, function(data){
+    showResults(data.Search);
+  });
+}
+
+function showResults(results){
+  var html = "";
+  $.each(results, function(index,value){
+    html += '<p>' + value.Title + '</p>';
+    console.log(value.Title);
+  });
+  $('#search-results').html(html);
+}
